2de6622621585aef5c3a352536f386c84a532b85
Xylitolwhc <whcshanghai@gmail.com>
2019-03-24 11:11:31 +0800

    Task3 Finished

--- Task3/Task3.py
+++ Task3/Task3.py
@@ -1,7 +1,6 @@
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 import pandas as pd
-from sklearn import svm
 import datetime
 
 # 决策树作业 No.3
@@ -60,18 +59,23 @@ for i in range(10):
         for label in testLabels:
             testLabelsFile.write(str(label))
             testLabelsFile.write("\n")
+
 '''
 # 留出法测试
-X_train, X_test, Y_train, Y_test = train_test_split(trainDatas, trainLabes, test_size=0.1, random_state=None)
+X_train, X_test, Y_train, Y_test = train_test_split(trainDatas, trainLabes, test_size=0.7, random_state=None)
 for j in range(20):
     minImpurityDecrease = 0.00005 * j
-    clf = tree.DecisionTreeClassifier(  # 定义决策树分类器
-        criterion="gini",  # 使用基尼系数作为信息增益的计算标准
-        splitter="best",  # 在特征的所有划分点中找出最优的划分点
-        min_impurity_decrease=minImpurityDecrease,  # 设置节点信息增益阈值，当小于此值时即停止划分节点
-        max_features=None);  
-    clf.fit(X_train, Y_train)
-    print("%.5f" % minImpurityDecrease, ":", "%.5f" % clf.score(X_test,Y_test))
+    score = 0
+    for i in range(5):
+        clf = tree.DecisionTreeClassifier(  # 定义决策树分类器
+            criterion="gini",  # 使用基尼系数作为信息增益的计算标准
+            splitter="best",  # 在特征的所有划分点中找出最优的划分点
+            min_impurity_decrease=minImpurityDecrease,  # 设置节点信息增益阈值，当小于此值时即停止划分节点
+            max_features=None);
+        clf.fit(X_train, Y_train)
+        score += clf.score(X_test, Y_test)
+    print("%.5f" % minImpurityDecrease, ":", "%.5f" % (score / 5.0), "%")
 '''
+
 endTime = datetime.datetime.now()
 print((endTime - startTime).seconds)  # 输出运行时间
